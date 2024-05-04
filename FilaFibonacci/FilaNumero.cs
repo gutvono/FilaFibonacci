fec07f3799d4f9f7b88c5dc1e1e4e142c2ce2cc4
Gustavo <guvono4@gmail.com>
2024-05-04 12:00:47 -0300

    Add project files.

---
+++ FilaFibonacci/FilaNumero.cs
@@ -0,0 +1,51 @@
+﻿using PilhaFibonacci;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilaFibonacci
+{
+    internal class FilaInteiro
+    {
+        Numero? head;
+        Numero? tail;
+
+        public FilaInteiro()
+        {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void Push(Numero aux)
+        {
+            if (IsEmpty()) head = tail = aux;
+            else
+            {
+                tail!.SetProximo(aux);
+                tail = aux;
+            }
+        }
+
+        bool IsEmpty() { return head == null && tail == null; }
+
+        public void Print()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine($"Fila vazia! Pressione qualquer tecla para continuar...");
+                Console.ReadKey();
+            }
+            else
+            {
+                Numero aux = this.head!;
+                while (aux != null)
+                {
+                    Console.WriteLine(aux.PrintN());
+                    aux = aux.GetProximo()!;
+                }
+            }
+        }
+    }
+}
---
+++ FilaFibonacci/Numero.cs
@@ -0,0 +1,19 @@
+﻿namespace PilhaFibonacci
+{
+    internal class Numero
+    {
+        int N;
+        Numero? Proximo;
+
+        public Numero(int n)
+        {
+            this.N = n;
+            this.Proximo = null;
+        }
+
+        public void SetProximo(Numero num) { this.Proximo = num; }
+        public Numero? GetProximo() { return this.Proximo; }
+        public int GetN() { return this.N; }
+        public string PrintN() { return $"Numero: {this.N}"; }
+    }
+}
